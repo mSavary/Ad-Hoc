f8c9a252a4061ad8ba40901af8fe0832e0d73a48
coco <coco@localhost.localdomain>
2014-03-26 17:24:24 +0100

    ajout des commentaires

--- Linux_v6/src/Tc.h
+++ Linux_v6/src/Tc.h
@@ -59,29 +59,105 @@ class Tc: public Message {
 private:
 	uint16_t mANSN;
 	uint16_t mReserved;
-	IPv6 *mFromIp;/*!< sender of the paquet */
+	IPv6 *mFromIp;/*!< sender of the packet */
 	std::list<IPv6*> mAdvertisedNeighborMainAddress;
 
 public:
+	/*!
+	 * \fn 	Tc(uint16_t packetSequenceNumber, IPv6 * originatorAddress, uint16_t messageSequenceNumber, std::list<IPv6*> advertisedList)
+	 * \brief Construct a Tc message with partial informations
+	 *
+	 * 	\param partial informations
+	 */
 	Tc(uint16_t packetSequenceNumber, IPv6 * originatorAddress,
 			uint16_t messageSequenceNumber, std::list<IPv6*> advertisedList);
+
+	/*!
+	 * \fn 	Tc(uint16_t packetLength, uint16_t packetSequenceNumber, uint8_t messageType, uint8_t vTime, uint16_t messageSize, IPv6 * originatorAddress, uint8_t timeToLive, uint8_t hopCount, uint16_t messageSequenceNumber,uint16_t Ansn, std::string ip, std::list<IPv6*> advertisedList)
+	 * \brief Construct a Tc message with a complete informations
+	 *
+	 * 	\param complete informations
+	 */
 	Tc(uint16_t packetLength, uint16_t packetSequenceNumber,
 			uint8_t messageType, uint8_t vTime, uint16_t messageSize,
 			IPv6 * originatorAddress, uint8_t timeToLive, uint8_t hopCount,
 			uint16_t messageSequenceNumber,uint16_t Ansn, std::string ip,
 			std::list<IPv6*> advertisedList);
 
+	/*!
+	 * \fn uint16_t getANSN()
+	 *
+	 * \brief Return the Advertised Neighbor Sequence Number of the Tc field
+	 */
 	uint16_t getANSN();
+
+	/*!
+	 * \fn uint16_t getReserved()
+	 *
+	 * \brief Return value of the field reserved
+	 */
 	uint16_t getReserved();
+
+	/*!
+	 * \fn 	void setReserved(uint16_t res)
+	 * \brief Set the value of the reserved field
+	 *
+	 * 	\param value of the field
+	 */
 	void setReserved(uint16_t res);
+
+	/*!
+	 * \fn std::list<IPv6*> getAdvertisedNeighborMainAddress()
+	 *
+	 * \brief Return the list of the AdvertisedNeighborMainAddress
+	 */
 	std::list<IPv6*> getAdvertisedNeighborMainAddress();
+
+	/*!
+	 * \fn 	void setANSN(uint16_t n)
+	 * \brief Set the value of the Advertised Neighbor Sequence Number field
+	 *
+	 * 	\param value of the field
+	 */
 	void setANSN(uint16_t n);
+
+	/*!
+	 * \fn inline IPv6* getForwarder()
+	 *
+	 * \brief Return the IP of the originator of the packet
+	 */
 	inline IPv6* getForwarder(){
 		return mFromIp;
 	}
+
+	/*!
+	 * \fn void	addAdvertisedNeighborMainAddress(short n, short o, short p, short q)
+	 * \brief Set the IP of the AdvertisedNeighborMainAddress
+	 *
+	 * 	\param the 4 last values of the IPv6 address
+	 */
 	void addAdvertisedNeighborMainAddress(short n, short o, short p, short q);
+
+	/*!
+	 * \fn void	addAdvertisedNeighborMainAddress(IPv6* n)
+	 * \brief Set the IP of the AdvertisedNeighborMainAddress
+	 *
+	 * 	\param the IPv6 addressof the AdvertisedNeighborMainAddress
+	 */
 	void addAdvertisedNeighborMainAddress(IPv6* n);
+
+	/*!
+	 * \fn void printData()
+	 *
+	 * \brief Print all the field of the Tc class
+	 */
 	void printData();
+
+	/*!
+	 * \fn int sendTc()
+	 *
+	 * \brief Send the Tc message with the values set in the fields
+	 */
 	int sendTc();
 };
 #endif
